95e00da48ed0ef8bea6a2cc38d3a5eec3dbbda4b
“parkhyunjae”  <phj2131@naver.com>
2022-02-03 18:14:45 +0900

    로그인성공

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,25 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    })
+        .then((res) => res.json())
+        .then((res) => console.log(res));
+}
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,46 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");    
+const { render } = require("../../../app");
+
+const users = {
+    id: ["main", "roqkf"],
+    psword: ["1234", "1234"]
+}
+
+const output = {
+    home : (req, res) => {
+        res.render("home/index");    
+    },
+    login : (req, res) => {
+        res.render("home/login");      
+    },
+}; 
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+        psword = req.body.psword;
+        
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                    msg: "로그인 성공 !!",
+                });    
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인 실패 !!",
+         });
+    },
 };
 
-const login = (req, res) => {
-    res.render("home/login");      
-};
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };
 
