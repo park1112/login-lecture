43b8f263d6a6e9a30b85f89dcf71aa706f68ec7e
“parkhyunjae”  <phj2131@naver.com>
2022-02-03 21:24:41 +0900

    fs모듈을 통한 로그인구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,9 @@
 "use strict";
+const fs = require("fs").promises;
 
 class UserStorage {
-    static #users = {
-        id: ["main", "roqkf"],
-        psword: ["1234", "1234"],
-        name : ["바보", "하하하"],
-    };
-
     static getUsers(...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => { 
             if (users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -20,18 +15,30 @@ class UserStorage {
     }
 
     static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
+        // const users = this.#users;
+
+        return fs.readFile("./src/databases/client/users.json")
+            .then((data) => {
+                return this.#getUserInfo(data, id);
+        })
+        .catch(console.error);
+    }
+
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
+                const idx = users.id.indexOf(id);
+                const usersKeys = Object.keys(users);
+                const userInfo = usersKeys.reduce((newUser, info) => {
+                    newUser[info] = users[info][idx];
+                    return newUser;
         }, {});
+                    
         return userInfo;
+
     }
 
     static save(userInfo){
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.pasword.push(userInfo.pasword);
